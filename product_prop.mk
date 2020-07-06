6237f97caac877f0a6a5394b55ecbf06dc39aad5
Akash <akashniki@gmail.com>
2020-07-06 23:07:52 +0200

    raphael: Enable fluence voice recording
    * This fixes google voice recorder
    * Also include prebuilt Google Recorder

--- product_prop.mk
+++ product_prop.mk
@@ -20,4 +20,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
-    debug.stagefright.omx_default_rank.sw-audio=16
+    debug.stagefright.omx_default_rank.sw-audio=16 \
+    persist.vendor.audio.fluence.voicerec=true
