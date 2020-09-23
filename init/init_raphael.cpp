e7c8c8011c8adb092f3deac5390ec04a9e4f8865
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-09-23 16:12:21 +0000

    raphael: init: Remove deprecated property
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -20,7 +20,6 @@
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
 
 void property_override(char const prop[], char const value[])
