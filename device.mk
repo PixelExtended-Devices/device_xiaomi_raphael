ee024134500ac2e8adb5a9e63107c9d385ffe76f
nato6613 <normandija1945@gmail.com>
2021-07-09 14:36:56 +0000

    raphael: Add pixel_2016_exclusive.xml
     - This allows us to use unlimited backup in Google Photos infinitely.

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     Snap
 endif
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
