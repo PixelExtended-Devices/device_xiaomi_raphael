2a4f11822d6d49b9532ce5f7796e8f805bc76cd0
ralf979 <ralf979@gmail.com>
2022-09-16 13:50:27 +0000

    PEX Bringup

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -12,29 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
+# Inherit from raphael device.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common VoltageOS stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
-
-# Official-ify
-VOLTAGE_BUILD_TYPE := OFFICIAL
-
-# UDFPS animations
-EXTRA_UDFPS_ANIMATIONS := true
-
-# Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 2160
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := voltage_raphael
+PRODUCT_NAME := aosp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="raphael" \
     PRODUCT_NAME="raphael"
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
