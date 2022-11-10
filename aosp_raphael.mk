50a56da735ba7142d7b15ff1313c366b8e7d299a
ralf979 <ralf979@gmail.com>
2022-11-10 11:25:57 +0000

    raphael: Use Common full Phonee

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common PEX stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 FACE_UNLOCK_SUPPORTED := true
