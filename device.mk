9f5eeb1b8fc3ca8c52d32e5b78e5a0011f2365b6
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-14 13:29:04 +0800

    raphael: Build sensors.udfps

--- device.mk
+++ device.mk
@@ -436,6 +436,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
+    sensors.udfps \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@2.1 \
     android.frameworks.sensorservice@1.0 \
